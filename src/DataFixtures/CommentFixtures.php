95501d733e903c89a8a92595ebb8b0f07f837612
Jensone <hello@jensone.com>
2025-05-21 12:02:01 +0200

    Instruction exo

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class CommentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::class;
+
+        // Récupération des utilisateurs nouvellement créés
+        $users = [];
+        for ($i=0; $i < 100; $i++) { 
+            $users[] = $this->getReference('USER_' . $i, User::class);
+        }
+        
+        // TODO: Régidiger les création de commentaires pour les articles
 
         $manager->flush();
     }
