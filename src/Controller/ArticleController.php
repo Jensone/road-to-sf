e78a9ebaf158fe213f87677e657aab598374e8d8
Jensone <hello@jensone.com>
2025-05-23 11:38:31 +0200

    Add KnpPaginatorBundle and implement pagination in ArticleController: Enhance article listing with pagination functionality

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,20 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/article')]
 final class ArticleController extends AbstractController
 {
     // Route "/articles" menant à la liste des articles
     #[Route('s', name: 'articles', methods: ['GET'])]
-    public function index(ArticleRepository $ar): Response
-    {
+    public function index(
+        ArticleRepository $ar, // Repository de l'entité Article
+        PaginatorInterface $paginator, // Classe pour la fonctionnalité de pagination
+        Request $request // Classe pour récupérer les paramètres de la requête HTTP
+    ): Response {
+        $all = $ar->findAll();
+        $pagination = $paginator->paginate(
+            $all,
+            $request->query->getInt('page', 1),
+            12
+        );
+
         return $this->render('article/index.html.twig', [
             'controller_name' => 'INDEX',
-            'articles' => $ar->findAll()
+            'articles' => $pagination
         ]);
     }
 }
