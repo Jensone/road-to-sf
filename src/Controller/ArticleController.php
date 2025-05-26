1f91fa6ea767d636f58dcf940d544c3ed3550d68
Jensone <hello@jensone.com>
2025-05-26 09:54:33 +0200

    Install Paginator, View route, Edit route, ArticleForm added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,21 +2,56 @@
 
 namespace App\Controller;
 
+use App\Form\ArticleForm;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/article')]
 final class ArticleController extends AbstractController
 {
+    public function __construct(
+        private ArticleRepository $ar,
+        EntityManagerInterface $em
+        ){}
+
     // Route "/articles" menant à la liste des articles
     #[Route('s', name: 'articles', methods: ['GET'])]
-    public function index(ArticleRepository $ar): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $query = $this->ar->findBy(['is_published' => true, 'is_archived' => false], ['id' => 'DESC']);
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'INDEX',
-            'articles' => $ar->findAll()
+            'articles' => $pagination
+        ]);
+    }
+
+    // Route "/article" menant à un article
+    #[Route('/{slug}', name: 'article', methods: ['GET'])]
+    public function view(string $slug): Response
+    {
+        return $this->render('article/view.html.twig', [
+            'article' => $this->ar->findOneBySlug($slug)
+        ]);
+    }
+
+    // Route "/article/{slug}/edit" menant à la modification d'un article
+    #[Route('/{slug}/edit', name: 'article_edit', methods: ['GET', 'POST'])]
+    public function edit(string $slug, Request $request): Response
+    {
+        $article = $this->ar->findOneBySlug($slug); // Récupération de l'article
+        $form = $this->createForm(ArticleForm::class, $article); // Mise en place du formulaire
+    
+        return $this->render('article/edit.html.twig', [
+            'articleForm' => $form // Envoi du formulaire à la vue
         ]);
     }
 }
