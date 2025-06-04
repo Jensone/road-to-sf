234858d7e550c920b01d7a9ae0bca87cc8881079
Jensone <hello@jensone.com>
2025-06-04 11:24:06 +0200

    Add upload folder configuration and implement upload logic in UploadService with error handling for file operations

--- src/Service/UploadSerivce.php
+++ src/Service/UploadSerivce.php
@@ -2,8 +2,39 @@
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
+/**
+ * Class UploadSerivce
+ * namespace App\Service
+ * Permet de gérer les téléversements dans l'application
+ */
 class UploadSerivce
 {
+
+    public function __construct(
+        private ParameterBagInterface $params,
+    ){}
+
+        public function upload(UploadedFile $file, string $type): string
+        {
+            $typeArr= [ // Tableau de ref pour les dossiers en focntion des types de fichiers
+                'image' => $this->params->get('upload_folder') . '/images', 
+                'document' => $this->params->get('upload_folder') . '/docs', 
+                'other' => $this->params->get('upload_folder')
+            ];
+
+            try {
+                $filename = uniqid($type . '-') . '.' . $file->guessExtension(); // Nom généré
+                $file->move($typeArr[$type], $filename); // Déplacement du fichier
+            } catch (\Exception $err) {
+                return $err->getMessage();
+            }
+
+            return $filename; // Retourne le nom du fichier
+        }
+
+
 
 }
